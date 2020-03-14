4d970e00b7ff0a1409caaa4543026a9fdc4837eb
MH-zhang <2106869096@qq.com>
2020-03-14 15:09:09 +0800

    add somthing

--- app/src/main/java/com/swufe/homework/MainActivity.java
+++ app/src/main/java/com/swufe/homework/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //add something
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
